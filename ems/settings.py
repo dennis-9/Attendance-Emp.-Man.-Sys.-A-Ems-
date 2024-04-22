329d756c65f1111d43f761c17dadb3ec814607a9
Dennis <asiedudennis30@gmail.com>
2024-04-22 17:52:46 +0000

    Deploying process

--- ems/settings.py
+++ ems/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--w(dug*va_megpa25w$gmv8o6+f8!fg_*m1^u$)z25pab0hicm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 CSRF_TRUSTED_ORIGINS = ["http://*,on-acorn.io", "https://*,on-acorn.io"]
 
@@ -84,15 +84,7 @@ WSGI_APPLICATION = 'ems.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'mysql': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MARIADB_DATABASE"),
-        'USER': os.getenv("MARIADB_USER"),
-        'PASSWORD': os.getenv("MARIADB_ROOT_PASSWORD"),
-        'HOST': os.getenv("MARIADB_HOST"),
-        'PORT': os.getenv("MARIADB_PORT", 3306),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
