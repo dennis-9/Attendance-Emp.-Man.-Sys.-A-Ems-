89fb7ffe807252c53826d249c3ed2f0159ab1a58
Dennis <asiedudennis30@gmail.com>
2024-05-07 15:07:46 +0000

    Create a new Vercel Deploy

--- ems/settings.py
+++ ems/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--w(dug*va_megpa25w$gmv8o6+f8!fg_*m1^u$)z25pab0hicm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
@@ -84,17 +84,17 @@ WSGI_APPLICATION = 'ems.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
 
 #AWS Database with RDS
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         
@@ -109,7 +109,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 
 # Render PostgreSQL Database
 # import dj_database_url
