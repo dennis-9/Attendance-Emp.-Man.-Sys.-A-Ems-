92419195d673db5a9cab1025ff4450df99c36330
Dennis <asiedudennis30@gmail.com>
2024-05-09 07:25:37 +0000

    Process Deploy to vercel 1

--- ems/settings.py
+++ ems/settings.py
@@ -163,6 +163,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (
